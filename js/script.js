147ac5e138df4d265d343ee5a3a8542c0d175ac7
Yasmin <yasminmutiarabintang@gmail.com>
2023-06-09 09:06:34 +0700

    first push

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .flex .navbar');
+let search = document.querySelector('.search-box');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
 }
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
